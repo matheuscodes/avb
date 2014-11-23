c2dfa11a82c37712e043b8427654ec614227c062
Matheus Borges Teixeira <matheus.bt@gmail.com>
2014-11-23 15:41:26 +0100

    Documenting AVBApp.

--- src/org/arkanos/avb/AVBApp.java
+++ src/org/arkanos/avb/AVBApp.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright (C) 2014 Matheus Borges Teixeira
+ * 
+ * This is a part of Arkanos Vocabulary Builder (AVB)
+ * AVB is an Android application for improving vocabulary.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.arkanos.avb;
 
 import org.arkanos.avb.data.BabelTower;
@@ -7,7 +26,12 @@ import org.arkanos.avb.data.LanguageSettings;
 import android.app.Application;
 import android.util.Log;
 
-// TODO rename class
+/**
+ * Ensures proper initialization.
+ * 
+ * @version 1.0
+ * @author Matheus Borges Teixeira
+ */
 public class AVBApp extends Application {
 	public static final String TAG = "AVB-";
 	private static boolean failed = false;
@@ -26,6 +50,11 @@ public class AVBApp extends Application {
 		}
 	}
 
+	/**
+	 * Checks whether initialization has failed.
+	 * 
+	 * @return true when there were problems.
+	 */
 	public static boolean failedInitialize() {
 		return failed;
 	}
